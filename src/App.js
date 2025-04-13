050aaabf38a2b5c1d4183377134f60fc0f6ba52a
Vikst009 <96147918+Vikst009@users.noreply.github.com>
2025-04-13 21:42:45 +0200

    create bill and tip calculator using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TipCalculator />
     </div>
   );
+}
+
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
+  const totalTip = bill * ((tip + friendTip) / 2 / 100);
+  console.log(friendTip);
+  return (
+    <div>
+      <Bill children="How much was the bill?" bill={bill} setBill={setBill} />
+      <Tip children="How did you like the srvice?" tip={tip} setTip={setTip} />
+      <Tip
+        children="How did your friend like the service?"
+        tip={friendTip}
+        setTip={setFriendTip}
+      />
+      <Output bill={bill} totalTip={totalTip} />
+      <ResetButton
+        setBill={setBill}
+        setTip={setTip}
+        setFriendTip={setFriendTip}
+      />
+    </div>
+  );
+}
+
+function Bill({ children, setBill, bill }) {
+  return (
+    <div>
+      <p>{children}</p>
+      <input
+        type="text"
+        onChange={(e) => setBill(Number(e.target.value))}
+        value={bill}
+        placeholder="Insert bill value"
+      />
+    </div>
+  );
+}
+
+function Tip({ children, tip, setTip }) {
+  return (
+    <div>
+      <p>{children}</p>
+      <select value={tip} onChange={(e) => setTip(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+function Output({ bill, tip, totalTip }) {
+  return (
+    <h3>
+      You pay ${bill + totalTip} ($ {bill === "" ? 0 : bill} + $ {totalTip})
+    </h3>
+  );
+}
+
+function ResetButton({ setBill, setTip, setFriendTip }) {
+  function handleReset() {
+    setBill("");
+    setTip(0);
+    setFriendTip(0);
+  }
+  return <button onClick={handleReset}>Reset</button>;
 }
 
 export default App;
